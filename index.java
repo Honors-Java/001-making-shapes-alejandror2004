6154b2100269d6d58445820bd6bfa6ac1d94f4ef
TUXEDO FALCON <alerod0204@gmail.com>
2020-09-13 21:01:44 +0000

    Compared to the original army tank, I connected the tom rectangle closer to the bottom rectangle.

--- index.java
+++ index.java
@@ -14,6 +14,15 @@ void draw() {
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
   rect(200, 200, 100, 50);
+  rect(230, 260, 160, 70);
+  rect(315, 190, 130, 20);
+  
+  ellipse(150, 295, 30, 30);
+  ellipse(180, 295, 30, 30);
+  ellipse(210, 295, 30, 30);
+  ellipse(240, 295, 30, 30);
+  ellipse(270, 295, 30, 30);
+  ellipse(300, 295, 30, 30);
   
   
  
